44f84877f47ac76aa4bc50ff513ab787dd1bf39c
Egor Stepanenko <st3pegor@gmail.com>
2025-10-23 10:27:32 +0700

    update migration

--- tree-generator-api/src/main/resources/db/changelog/001-init.sql
+++ tree-generator-api/src/main/resources/db/changelog/001-init.sql
@@ -1,18 +1,6 @@
 --liquibase formatted sql
 --changeset StepanenkoES:001
 
--- Таблица ролей
-CREATE TABLE api_roles
-(
-    id          SERIAL PRIMARY KEY,
-    name        VARCHAR(30) NOT NULL UNIQUE,
-    description TEXT
-);
-
-COMMENT ON TABLE api_roles IS 'Роли пользователей и их описания (admin, analyst, guest).';
-COMMENT ON COLUMN api_roles.name IS 'Название роли: admin, analyst, guest.';
-COMMENT ON COLUMN api_roles.description IS 'Текстовое описание прав и обязанностей роли.';
-
 -- Таблица пользователей
 CREATE TABLE api_user
 (
@@ -21,7 +9,6 @@ CREATE TABLE api_user
     email         VARCHAR(100) UNIQUE,
     password_hash TEXT        NOT NULL,
     full_name     VARCHAR(100),
-    role_id       INT         REFERENCES api_roles (id) ON DELETE SET NULL,
     created_at    TIMESTAMP DEFAULT now()
 );
 
@@ -30,7 +17,6 @@ COMMENT ON COLUMN api_user.login IS 'Логин пользователя для 
 COMMENT ON COLUMN api_user.email IS 'Электронная почта пользователя.';
 COMMENT ON COLUMN api_user.password_hash IS 'Хэш пароля (bcrypt/argon2 и т.п.).';
 COMMENT ON COLUMN api_user.full_name IS 'Полное имя пользователя.';
-COMMENT ON COLUMN api_user.role_id IS 'Ссылка на роль пользователя (roles.id).';
 COMMENT ON COLUMN api_user.created_at IS 'Дата и время создания учётной записи.';
 
 -- Таблица проектов
@@ -41,32 +27,33 @@ CREATE TABLE project
     title         VARCHAR(100) NOT NULL,
     description   TEXT,
     created_at    TIMESTAMP DEFAULT now(),
-    last_modified TIMESTAMP DEFAULT now(),
+    updated_at    TIMESTAMP DEFAULT now(),
     is_public     BOOLEAN   DEFAULT false
 );
 CREATE INDEX idx_project_owner ON project (owner_id);
 COMMENT ON TABLE project IS 'Проекты симуляций (группа работ, набор симуляций).';
 COMMENT ON COLUMN project.is_public IS 'Флаг публичности проекта (доступен гостям).';
 
--- Таблица симуляций
-CREATE TABLE simulation
+-- Таблица ролей
+CREATE TABLE project_roles
 (
     id          SERIAL PRIMARY KEY,
     project_id  INT REFERENCES project (id) ON DELETE CASCADE,
-    started_by  INT REFERENCES api_user (id) ON DELETE SET NULL,
-    status      VARCHAR(20) DEFAULT 'pending',
-    started_at  TIMESTAMP   DEFAULT now(),
-    finished_at TIMESTAMP
+    name        VARCHAR(30) NOT NULL UNIQUE,
+    description TEXT
 );
-CREATE INDEX idx_simulation_project ON simulation (project_id);
-COMMENT ON TABLE simulation IS 'Фиксированные запуски симуляций (копия параметров и путь к результатам).';
+CREATE INDEX idx_api_roles_project_id ON project_roles (project_id);
+
+COMMENT ON TABLE project_roles IS 'Роли пользователей и их описания (admin, analyst, guest).';
+COMMENT ON COLUMN project_roles.name IS 'Название роли: admin, analyst, guest.';
+COMMENT ON COLUMN project_roles.description IS 'Текстовое описание прав и обязанностей роли.';
 
 -- Таблица шаблонов
 CREATE TABLE template
 (
     id            SERIAL PRIMARY KEY,
     user_id       INT          REFERENCES api_user (id) ON DELETE SET NULL,
-    simulation_id INT          REFERENCES simulation (id) ON DELETE SET NULL,
+    simulation_id INT          REFERENCES project (id) ON DELETE SET NULL,
     name          VARCHAR(100) NOT NULL,
     description   TEXT,
     parameters    JSONB,
@@ -75,6 +62,19 @@ CREATE TABLE template
 CREATE INDEX idx_template_user ON template (user_id);
 COMMENT ON TABLE template IS 'Сохранённые шаблоны параметров симуляций для быстрого применения.';
 
+
+-- Таблица симуляций
+CREATE TABLE simulation
+(
+    id          SERIAL PRIMARY KEY,
+    template_id  INT REFERENCES template (id) ON DELETE CASCADE,
+    user_id     INT REFERENCES api_user (id) ON DELETE SET NULL,
+    status      VARCHAR(20) DEFAULT 'pending',
+    started_at  TIMESTAMP   DEFAULT now(),
+    finished_at TIMESTAMP
+);
+CREATE INDEX idx_simulation_template ON simulation (template_id);
+COMMENT ON TABLE simulation IS 'Фиксированные запуски симуляций (копия параметров и путь к результатам).';
 
 -- Таблица результатов симуляции
 CREATE TABLE simulation_result
