6b44bf06e2b5e80e98704597445c9ecd61c897a1
Egor Stepanenko <st3pegor@gmail.com>
2025-11-23 23:47:52 +0700

    project endpoint

--- tree-generator-api/src/main/java/ru/tusur/asu/tasc/tree_generator_api/repository/ProjectRepository.java
+++ tree-generator-api/src/main/java/ru/tusur/asu/tasc/tree_generator_api/repository/ProjectRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.tusur.asu.tasc.tree_generator_api.entity.Project;
 
+import java.util.Optional;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    Optional<Project> findByTitle(String title);
 }
